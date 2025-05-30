dbd5546d4cf119c1064d56a3c55da2f0f2ca0fef
Heri Rama <heringatoluwa@outlook.com>
2025-05-30 22:39:10 +0300

    update

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+import os
+
 class Config:
-  SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://DESKTOP-8QPF538/terra_herb?driver=ODBC+Driver+17+for+SQL+Server"
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   SESSION_PERMANENT = False
   SESSION_TYPE = "filesystem"

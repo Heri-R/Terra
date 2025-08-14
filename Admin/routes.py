050af86c6e856e5ca401c07447857cb352d4e855
Heri Rama <heringatoluwa@outlook.com>
2025-08-14 23:04:31 +0300

    update

--- Admin/routes.py
+++ Admin/routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, Blueprint, render_template, redirect, url_for, flash, request, jsonify, session, make_response
 from flask_caching import Cache, CachedResponse
-from flask_redis import FlaskRedis
+
 from flask_login import login_required, fresh_login_required
 from Models.base_model import db, get_local_time
 from Models.users import Role
@@ -20,14 +20,12 @@ from decorator import role_required, branch_required
 from collections import defaultdict
 from sqlalchemy.sql import func, desc
 from slugify import slugify
-from urllib.parse import urlparse
-import os, redis
+
 
 admin = Blueprint("admin", __name__)
 cache = Cache()
-redis_client = FlaskRedis()
-url = urlparse(os.environ.get("REDIS_URL"))
-r = redis.Redis(host=url.hostname, port=url.port, password=url.password, ssl=(url.scheme == "rediss"), ssl_cert_reqs=None)
+
+
 
 region_districts = {
   "Arusha": ["Monduli", "Arusha", "Arumeru", "Karatu", "Longido", "Ngorongoro"],

73323923e7dccab685951abefe824449acf2e99b
Heri Rama <heringatoluwa@outlook.com>
2025-10-14 22:29:37 +0300

    update

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 load_dotenv(override=True)
 
 class Config:
-  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://", 1)
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   SESSION_PERMANENT = False
   SESSION_TYPE = "filesystem"

e0ca892a46df839534437fd0b44bbe433bd52cd2
Heri Rama <heringatoluwa@outlook.com>
2025-11-07 22:05:23 +0300

    update

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ import redis
 load_dotenv(override=True)
 
 class Config:
-  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://", 1)
+  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   SESSION_PERMANENT = False
   SESSION_TYPE = "filesystem"
   SECRET_KEY = os.environ.get("SECRET_KEY")
   CACHE_TYPE = "RedisCache"
-  CACHE_REDIS_URL = os.environ.get("CACHETOGO_URL")
+  CACHE_REDIS_URL = os.environ.get("REDIS_URL")
  
 
